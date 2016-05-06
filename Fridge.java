9e60c2eaaea75a95fa7acc7877360cfbd5ae07e3
cassiealyce <cassie_warren27@mymail.eku.edu>
2016-05-06 14:53:11 -0400

    Update Fridge.java

--- Fridge.java
+++ Fridge.java
@@ -17,6 +17,8 @@ import oracle.jdbc.OracleStatement;
  *
  * @author CassieAlyce
  */
+ /*The Fridge class tracks the ingredients that are in the fridge (ingredientsInFridge) and holds the functions relevant to 
+ the fridge such as addIngredient(), deleteIngredient(), etc.*/
 public class Fridge {
      ArrayList<Ingredient> ingredientsInFridge;
     Connection conn = null;
@@ -26,9 +28,11 @@ public class Fridge {
     Fridge(){
        ingredientsInFridge = new ArrayList<>(); 
     }
+    /*adds an ingredient to the fridge*/
     public void addIngredient(Ingredient newIngredient){
         ingredientsInFridge.add(newIngredient);
     }
+    //deletes an Ingredient from the fridge
     public void deleteIngredient(String ingredientName){
         for(Ingredient ingredient : ingredientsInFridge){
             if(ingredient.ingredient_name.equals(ingredientName)){
@@ -36,6 +40,7 @@ public class Fridge {
             }
     }
     }
+    //Increments the quantity of a fridge ingredient by one
     public void incrementFridgeIngredient(Ingredient selectedIngr){
         conn = ConnectDB.setupConnnection();
         try {
@@ -54,9 +59,10 @@ public class Fridge {
             ConnectDB.close(rs);
             ConnectDB.close(conn);
         }
-        populateFridgeList();
+        populateFridgeList(); //update the data in the list of fridge ingredients
 
     }
+    //decrements the quantity of a fridge ingredient by one.
      public void decrease(Ingredient selectedIngr){
          if(selectedIngr.quantity>0){
         conn = ConnectDB.setupConnnection();
@@ -77,8 +83,9 @@ public class Fridge {
             ConnectDB.close(conn);
         }
         }
-         populateFridgeList();
+         populateFridgeList();//update the data in the list of fridge ingredients
     }
+    /*Returns the ingredient object with the paramter ingredientName that is stored in the ingredientsInFridge array list*/
     public Ingredient getIngredient(String ingredientName){
         Ingredient selectedIngredient = new Ingredient();
         for (Ingredient ingredient : ingredientsInFridge) {
@@ -88,6 +95,7 @@ public class Fridge {
         }
         return selectedIngredient;
     }
+    /*populateFridgeList() returns a DefaultListModel containing the ingredients in the database whose quantity>0*/
     public DefaultListModel populateFridgeList(){
         conn = ConnectDB.setupConnnection();
         DefaultListModel model = new DefaultListModel();
@@ -109,7 +117,7 @@ public class Fridge {
                 String food_group = rs.getString("food_group");
                 int quantity = rs.getInt("quantity");
                 Ingredient newIngredient = new Ingredient(name, calories, protein, sugar, fat, sodium, food_group, quantity);
-                ingredientsInFridge.add(newIngredient);
+                ingredientsInFridge.add(newIngredient); //add ingredient to the ingredientsInFridge array
                 model.addElement(name);
 
             }
@@ -121,6 +129,7 @@ public class Fridge {
         }
         return model;
     }
+    /*populateIngredientsList() returns a DefaultListModel containing the of the ingredients from the database*/
      public DefaultListModel populateIngredientsList(){
         conn = ConnectDB.setupConnnection();
         DefaultListModel model = new DefaultListModel();
